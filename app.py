1bb8a0df370ce8e75a00a251c8e66d40f87834ad
Arijit Das <arijit10das2002@gmail.com>
2022-02-02 13:34:37 +0530

    initial

---
+++ app.py
@@ -0,0 +1,37 @@
+import pymongo
+from flask import Flask,render_template, request,make_response
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user = os.getenv("user")
+pswd = os.getenv("pswd")
+database = os.getenv("database")
+
+app=Flask(__name__)
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    
+    return render_template('index.html')
+
+@app.route('/form',methods=['POST','GET'])
+def form():
+    myclient = pymongo.MongoClient(f"mongodb+srv://{user}:{pswd}@{database}.gjpbh.mongodb.net/test?retryWrites=true&w=majority")
+    mydb = myclient["test"]
+    mycol = mydb["portfolio"]
+    name=request.form.get('name')
+    email=request.form.get('email')
+    subject=request.form.get('subject')
+    message=request.form.get('message')
+    print(name,email,subject,message)
+    list={"name":name,"email":email,"subject":subject,"message":message}
+    mycol.insert_one(list)
+    return render_template('index.html')
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
